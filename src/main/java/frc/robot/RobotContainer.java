2a86c497ddafb061292aa8a5ed6e7e71f0c1afd7
BeanieBot <team3492@gmail.com>
2024-01-27 11:21:28 -0500

    changes to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -50,9 +51,9 @@ public class RobotContainer {
       new RunCommand(() -> driveTrain.driveArcade(
         driveController.getLeftX(), 
         driveController.getRightY()),
-        driveTrain);
+        driveTrain)
 
-    )
+    );
   }
 
   /**
