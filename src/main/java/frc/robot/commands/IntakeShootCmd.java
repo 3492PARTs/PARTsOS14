42f2602d9772a29263b53aa026de9df31ab4a9b9
James <bucheckerr@gmail.com>
2024-02-10 15:12:13 -0500

    intake shoot cmd

---
+++ src/main/java/frc/robot/commands/IntakeShootCmd.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+public class IntakeShootCmd extends Command {
+  /** Creates a new RunIntakeCmd. */
+  Intake intake;
+  Shooter shooter;
+  public IntakeShootCmd() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = Intake.getInstance();
+    this.shooter = Shooter.getInstance();
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    shooter.runShooter(1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Shooter.shooterLeftLeader.getMotorOutputPercent() >= 0.98) {
+      new RunIntakeCmd(-1);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.runIntake(0);
+    shooter.runShooter(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
