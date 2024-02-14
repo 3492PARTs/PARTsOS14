f8660bec90740d4146c2729dde94dbbaa63fb9b6
BeanieBot <team3492@gmail.com>
2024-02-14 16:54:48 -0500

    intake code

--- src/main/java/frc/robot/commands/IntakeShootCmd.java
+++ src/main/java/frc/robot/commands/IntakeShootCmd.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
@@ -17,18 +18,21 @@ public class IntakeShootCmd extends Command {
     this.intake = Intake.getInstance();
     this.shooter = Shooter.getInstance();
     addRequirements(intake);
+    addRequirements(shooter);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooter.runShooter(1);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
+  //
   @Override
   public void execute() {
-    if (Shooter.shooterLeftLeader.getMotorOutputPercent() >= 0.98) {
+    shooter.runShooter(1);
+
+    if (Shooter.shooterLeftLeader.getSelectedSensorVelocity() / 6 >= 5800) {
       new RunIntakeCmd(-1);
     }
   }
